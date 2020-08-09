d7d5a62c3601d80e5f80a9eecb07114fff4a170c
KabirBhae <67532637+KabirBhae@users.noreply.github.com>
2020-08-09 20:57:35 +0600

    new md file added

---
+++ NullVsUndefined.js
@@ -0,0 +1,3 @@
+//variable declare kore value set na korle
+// function er return kichu na dile
+/* function a parameter kom/beshi pass korle*/
---
+++ hellobro.md
@@ -0,0 +1,3 @@
+variable এর ভ্যালু সেট না করলে
+function এ কোনো ভ্যালু return না করলে
+function এ argument কম/বেশি pass করলে
